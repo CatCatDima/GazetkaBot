7e6ff7ecae0f60747394917a2ec75a399bfc2846
CatCatDima <CatCatDima@yandex.ru>
2024-08-05 13:38:35 +0300

    Загрузил репу

---
+++ ads.py
@@ -0,0 +1,15 @@
+import _elementtree as xml
+
+class Ad:
+    def __init__(self,header:str,code):
+        self.header=header
+        self.code=code
+
+    def parse_code():
+        pass
+
+def search(parametrs:dict):
+    results=[]
+    
+
+    return results
---
+++ main.py
@@ -0,0 +1,36 @@
+import telebot
+from telebot import types
+import ads, designs, resources, users
+
+TOKEN=input("Введите API токен: ")
+bot=telebot.TeleBot(TOKEN)
+
+@bot.message_handler(['start','главное_меню'])
+def mainmenu(message):
+    markup=types.ReplyKeyboardMarkup(row_width=2,resize_keyboard=True)
+
+    search=types.KeyboardButton('/поиск')
+    recommendations=types.KeyboardButton('/рекомендации')
+    my_ads=types.KeyboardButton('/мои_объявления')
+    channels=types.KeyboardButton('/каналы')
+    settings=types.KeyboardButton('/Настройки')
+
+    markup.add(search,recommendations,my_ads,channels,settings)
+
+    bot.send_message(message.chat.id,'главное меню',reply_markup=markup)
+
+@bot.message_handler(['мои_объявления'])
+def myads(message):
+    markup=types.ReplyKeyboardMarkup(row_width=1,resize_keyboard=True)
+
+    markup.add(types.KeyboardButton('/главное_меню'))
+
+    for ad in ads.search({"user":message.from_user.id}):
+        markup.add(ad.header)
+
+    markup.add(types.KeyboardButton('/+новое_объявление+'))
+
+    bot.send_message(message.chat.id,'главное меню',reply_markup=markup)
+
+print("Бот запущен успешно!")
+bot.polling(True)
